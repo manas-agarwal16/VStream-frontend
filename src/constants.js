852fde37a2168b92b678f45e6ab94756b0148301
mana-educase <manas.agarwal@educase.io>
2025-05-19 07:24:10 +0530

    backend url update to life.com

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-// export const BACKEND_URL = 'https://vstream-jylj.onrender.com/api/v1' // manas.agarwal
-export const BACKEND_URL = 'https://vstream-backend.onrender.com/api/v1' // life.com
+export const BACKEND_URL = 'https://vstream-jylj.onrender.com/api/v1' // manas.agarwal
+// export const BACKEND_URL = 'https://vstream-backend.onrender.com/api/v1' // life.com
 // export const BACKEND_URL = 'http://localhost:8000/api/v1'
 // export const BACKEND_URL = 'https://vstream.twilightparadox.com/api/v1';
